9b2e65afd1a255fd4179c19f5a15f31fdf1bba8c
Dylan Schlager <mail@dylanschlager.com>
2022-02-05 11:39:48 -0700

    Adding time ago handling

--- lambda/handler.py
+++ lambda/handler.py
@@ -3,17 +3,18 @@ import json
 # import the AWS SDK (for Python the package name is boto3)
 import boto3
 # import two packages to help us with dates and date formatting
-from time import gmtime, strftime
+import time
 
 # create a DynamoDB object using the AWS SDK
 dynamodb = boto3.resource('dynamodb')
 # use the DynamoDB object to select our table
 table = dynamodb.Table('HelloWorldDatabase')
-# store the current time in a human readable format in a variable
-now = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
 
 # define the handler function that the Lambda service will use as an entry point
 def lambda_handler(event, context):
+    # store the current time in a human readable format in a variable
+    for_js = int(int(time.time())) * 1000
+    print(f"for_js: {for_js}")
 # extract values from the event object we got from the Lambda service and store in a variable
     try:
         name = event['firstName'] +' '+ event['lastName']
@@ -28,9 +29,13 @@ def lambda_handler(event, context):
         Item={
             'ID': name,
             'Bringing': bringing,
-            'Signed Up Time':now
+            'Signed Up Time':for_js
             })
 # return a properly formatted JSON object
+    if bringing != "":
+        bringstring = f" You are now bringing {bringing}."
+    else:
+        bringstring = ""
     return {
         'statusCode': 201,
         'headers': {
@@ -38,5 +43,5 @@ def lambda_handler(event, context):
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
         },
-        'body': json.dumps(f'Thanks, {name}. Your entry to bring {bringing} has been entered. Please refresh the page!')
+        'body': json.dumps(f'Welcome, {name}.{bringstring} Please refresh the page.')
     }
